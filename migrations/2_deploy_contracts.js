6216975a9f4a9580b9fa60d1620afedf710a48bd
ducchinhbka <thefuturedevvn@gmail.com>
2021-09-04 14:52:33 +0700

    fix migration

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,22 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(deployer, network, accounts) {
+  
+    // deploy dai token
+    await deployer.deploy(DaiToken)
+    const daiToken = await DaiToken.deployed()
+
+    // deploy dapp token
+    await deployer.deploy(DappToken)
+    const dappToken = await DappToken.deployed()
+
+    // deploy dapp token
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+    const tokenFarm = await TokenFarm.deployed()
+
+    dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+    daiToken.transfer(accounts[1], '100000000000000000000');
+};
